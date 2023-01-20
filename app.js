a562ed0e4eee8afe54e7bf894828a31414a7d510
lmkrenzDev <lkrenz94@gmx.de>
2023-01-20 20:34:43 +0100

    plzErkennung

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
-new Vue({
-    el: '#appVue',
-    data:{},
-    methods:{}
+// new Vue({
+//     el: '#appVue',
+//     data:{},
+//     methods:{}
+// })
+
+let plzOrt;
+let standortSpendezentrum = '50667'
+
+document.getElementById('switchAbholung').checked = false
+
+document.getElementById('switchAbholung').addEventListener('change', () => {
+    document.getElementById('adressEingabeForm').classList.toggle('d-none')
 })
 
-document.getElementById('switchAbholung').addEventListener('change', ()=> {
-    document.getElementById('')
+// read local JSON file in javascript
+fetch("./assets/plzOrt.json")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        plzOrt = data;
+
+    })
+
+
+document.getElementById('inputPlz').addEventListener('change', (event) => {
+    let eingabePlz = event.target.value
+
+    if (standortSpendezentrum.substring(0, 2) === eingabePlz.substring(0, 2)) {
+        if (plzOrt[eingabePlz]) {
+            document.getElementById('inputStadt').value = plzOrt[eingabePlz]['ort']
+        } else {
+            document.getElementById('inputStadt').value = 'Dies ist keine gültige PLZ'
+        }
+    } else {
+        document.getElementById('inputStadt').value = ''
+        document.getElementById('inputPlz').value = ''
+    }
 })

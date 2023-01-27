82472759b7657f7ea5435abbc36684bc53ae62a5
lmkrenzDev <lkrenz94@gmx.de>
2023-01-27 21:11:54 +0100

    Registrierung

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,68 @@
+'use strict'
 
-const App = new Vue({
-    el: '#appVue',
-    data: () => ({
-        abholung: false
-    }),
+let plzOrt;
+let standortSpendezentrum = '50667'
+
+// read local JSON file in javascript
+fetch("./assets/plzOrt.json")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        plzOrt = data;
+
+    })
+
+const {
+    createApp
+} = Vue
+
+createApp({
+    data() {
+        return {
+            abholung: false, 
+            wasValidated: false,
+            artKleiderspende: '',
+            krisengebiet: 'x',
+            vorname:'',
+            nachname:'',
+            strasse:'',
+            hausnummer:'',
+            plz:'',
+            ort:'',
+            eingabeAbholung:'Übergabe an der Geschäftsstelle',
+
+        }
+    },
     methods: {
         onChangeSwitchAbholung() {
-            this.abholung = this.abholung==true ? false : true;
-            console.log(this.abholung)
+            this.abholung = this.abholung == true ? false : true;
+            this.eingabeAbholung = this.abholung == true ? 'Abholung' : 'Übergabe an der Geschäftsstelle';
+        },
+        validation(){
+
+            
+
+            this.wasValidated = true;
+        },
+        changePlz(){
+            // let eingabePlz = event.target.value
+
+            if (standortSpendezentrum.substring(0, 2) === this.plz.substring(0, 2)) {
+
+                this.ort = plzOrt[this.plz] ? plzOrt[this.plz]['ort'] : 'Dies ist keine gültige PLZ';
+                // if (plzOrt[this.plz]) {
+                //     this.ort = plzOrt[this.plz]['ort']
+                // } else {
+                //     document.getElementById('inputStadt').value = 'Dies ist keine gültige PLZ'
+                // }
+            } else {
+                this.ort = ''
+                this.plz = ''
+            }
         }
     }
-})
+}).mount('#appVue')
+
+
+

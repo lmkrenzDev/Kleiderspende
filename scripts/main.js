4db5652a91971477afd08c2e68c6372e177f97ef
lmkrenzDev <lkrenz94@gmx.de>
2023-02-01 21:46:55 +0100

    Kommentierung

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@
 let plzOrt;
 let standortSpendezentrum = '50667'
 
-// read local JSON file in javascript
+// Einlesen der JSON-Datei mit allen PLZ und zugehörigen Orten und zwischenspeichern der JSON-Daten in der Variablen plzOrt
 fetch("./assets/plzOrt.json")
     .then(function (response) {
         return response.json();
@@ -13,6 +13,7 @@ fetch("./assets/plzOrt.json")
 
     })
 
+// Initialisierung von Vue.js
 const {
     createApp
 } = Vue
@@ -48,15 +49,17 @@ createApp({
             fehlermeldungPlz: '',
             datum: '',
             uhrzeit: ''
-
-
         }
     },
+    // Functions für die Stuerung der WEbanwenungsoberfläche
     methods: {
+        //Function für das Umschalten und entsprechende Ein- und Ausblenden der Eingabefelder im Formular 
         onChangeSwitchAbholung() {
             this.abholung = this.abholung == true ? false : true;
             this.eingabeAbholung = this.abholung == true ? 'Abholung' : 'Übergabe an der Geschäftsstelle';
         },
+        // Function für die Validierung der Eingaben 
+        // Es wird jeweils überprüft, ob die Eingaben vorhanden sind 
         validation() {
 
             this.validEingabeArtKleiderspende = this.artKleiderspende != '' ? true : false;
@@ -81,11 +84,11 @@ createApp({
                 this.fehlermeldungPlz = 'Bitte geben Sie eine gültige deutsche PLZ ein.'
                 this.invalidEingabePlz = true;
                 this.validEingabePlz = false;
-
+                // Bei der PLZ wird zusätzlich geprüft, ob die ersten beiden Ziffern den ersten beiden Ziffern des Spendenzentrums entsprechen
             } else if (standortSpendezentrum.substring(0, 2) === this.plz.substring(0, 2)) {
                 this.validEingabePlz = true;
                 this.invalidEingabePlz = false;
-
+                // Zusätzlich wird bei der PLZ geprüft, ob diese in Deutschland vorhanden ist bzw. ob ein Ort zu dieser PLZ gehört
             } else if (this.ort == '') {
                 this.fehlermeldungPlz = 'Bitte geben Sie eine gültige deutsche PLZ ein.'
                 this.invalidEingabePlz = true;
@@ -96,9 +99,11 @@ createApp({
                 this.validEingabePlz = false;
             }
 
+            // Überprüfen, ob alle Eingaben richitig sind, wenn Ja wird die Zusammenfassung angezeigt
             if (this.validEingabeArtKleiderspende && this.validEingabeKrisengebiet) {
                 this.wasValidated = true;
             }
+            /// Bei einer Abholdung an einer Abholadresse ist eine zusätzliche Überpfürung weiterer Eingabefelder notwendig
             if (this.abholung) {
                 if (this.validEingabeVorname && this.validEingabeNachname && this.validEingabeStrasse && this.validEingabeHausnummer && this.validEingabePlz) {
                     this.wasValidated = true;
@@ -107,10 +112,12 @@ createApp({
                 }
             }
 
+            //Zwischenspeichern der Registrierungszeit um diese in der Zusammenfassung anzuzeigen
             let dateNow = new Date()
             this.uhrzeit = dateNow.toLocaleTimeString('de-de');
             this.datum = dateNow.toLocaleDateString();
         },
+        //Function für das automatische Ausfüllen des Orts bei der Eingabe einer korrekten PLZ
         changePlz() {
 
             this.ort = plzOrt[this.plz] ? plzOrt[this.plz]['ort'] : '';

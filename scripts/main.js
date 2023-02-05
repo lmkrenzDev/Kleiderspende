0b0209f28cfcbf7163719743e374933a14521979
lmkrenzDev <lkrenz94@gmx.de>
2023-02-05 07:55:43 +0100

    vue Komponenten ergänzt

--- scripts/main.js
+++ scripts/main.js
@@ -13,43 +13,83 @@ fetch("./assets/plzOrt.json")
 
     })
 
-// Initialisierung von Vue.js
-const {
-    createApp
-} = Vue
+//Hearder und Footer-Komponeten aufbauen
+Vue.component('header-kk', {
+    template: `
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark pt-lg-3 sticky-top">
+            <div class="container">
+                <!-- Logo -->
+                <a class="navbar-brand fw-bold fs-2" href="/index.html">
+                    <img src="/assets/Logo.png" alt="Logo" width="40" height="40"
+                        class="d-inline-block align-text-top me-2">
+                    Kreiselkleider
+                </a>
+                <!-- Burger-Menü für kleine Endgeräte (wird auf großen Endgeräten nicht angezeigt)  -->
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <!-- Liste mit den einzelnen Menüpunkten -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0 fs-4">
+                        <li class="nav-item mx-lg-2">
+                            <a class="nav-link text-white" href="/index.html">Spende</a>
+                        </li>
+                        <li class="nav-item mx-lg-2">
+                            <a class="nav-link text-white" href="Spendenregionen.html">Spendenregionen</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+`
+});
 
-createApp({
-    data() {
-        return {
-            abholung: false,
-            wasValidated: false,
-            artKleiderspende: '',
-            krisengebiet: 'x',
-            vorname: '',
-            nachname: '',
-            strasse: '',
-            hausnummer: '',
-            plz: '',
-            ort: '',
-            eingabeAbholung: 'Übergabe an der Geschäftsstelle',
-            validEingabeArtKleiderspende: '',
-            invalidEingabeArtKleiderspende: '',
-            validEingabeKrisengebiet: '',
-            invalidEingabeKrisengebiet: '',
-            validEingabeVorname: '',
-            invalidEingabeVorname: '',
-            validEingabeNachname: '',
-            invalidEingabeNachname: '',
-            validEingabeStrasse: '',
-            invalidEingabeStrasse: '',
-            validEingabeHausnummer: '',
-            invalidEingabeHausnummer: '',
-            validEingabePlz: '',
-            invalidEingabePlz: '',
-            fehlermeldungPlz: '',
-            datum: '',
-            uhrzeit: ''
-        }
+Vue.component('footer-kk', {
+    template: `
+        <nav class="navbar fixed-bottom navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand fs-5" href="/index.html">&copy 2023 Kreiselkleider</a>
+                <a class="navbar-brand fs-5" href="impressum.html">Impressum</a>
+            </div>
+        </nav>
+        `
+});
+
+// Initialisierung von Vue.js
+let vueApp = new Vue({
+    el: '#appVue',
+    //Variablen, die in der Index.html dynamisch verwendet werden
+    data: {
+        abholung: false,
+        wasValidated: false,
+        artKleiderspende: '',
+        krisengebiet: 'x',
+        vorname: '',
+        nachname: '',
+        strasse: '',
+        hausnummer: '',
+        plz: '',
+        ort: '',
+        eingabeAbholung: 'Übergabe an der Geschäftsstelle',
+        validEingabeArtKleiderspende: '',
+        invalidEingabeArtKleiderspende: '',
+        validEingabeKrisengebiet: '',
+        invalidEingabeKrisengebiet: '',
+        validEingabeVorname: '',
+        invalidEingabeVorname: '',
+        validEingabeNachname: '',
+        invalidEingabeNachname: '',
+        validEingabeStrasse: '',
+        invalidEingabeStrasse: '',
+        validEingabeHausnummer: '',
+        invalidEingabeHausnummer: '',
+        validEingabePlz: '',
+        invalidEingabePlz: '',
+        fehlermeldungPlz: '',
+        datum: '',
+        uhrzeit: ''
     },
     // Functions für die Stuerung der WEbanwenungsoberfläche
     methods: {
@@ -123,4 +163,4 @@ createApp({
             this.ort = plzOrt[this.plz] ? plzOrt[this.plz]['ort'] : '';
         }
     }
-}).mount('#appVue')
+})
